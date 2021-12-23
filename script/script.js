c7f7d817084f34f6a3f23d138b3e48415dbd9c0b
kazam zahedi <kazam.zahedi@gmail.com>
2021-12-23 09:32:23 +0100

    bug fix slideshow

--- script/script.js
+++ script/script.js
@@ -3,17 +3,42 @@ $(document).ready(function() {
     var myCarousel = document.querySelector('#carouselExampleCaptions');
     var carousel = new bootstrap.Carousel(myCarousel, {
         interval: 10000,
+        
     });
     
-    var carouselItem = document.getElementsByClassName('carousel-item');
-    
-    carouselItem.classList.add('adf');
-    console.log("movein");
-    var carouselCaption = document.getElementsByClassName('.carousel-pation');
-    
-    if (condition) {
+    $(".carousel-caption").addClass("moveselector");
+    // const myTimeout = setTimeout(moveTitle, 10000);
+    // var carouselItem = docoment.querySelector('.carousel-item');
+    // function moveTitle() {
+
+    //     // if (carouselItem == "active") {
+    //     //     carouselItem.style.opacity = "0.1"; 
+    //     //     console.log("hi");
+    //     // }
+    //     console.log("hi");
+
         
-    }
+
+
+    // for (let index = 0; index < array.length; index++) {
+    //     const element = array[index];
+            
+    // }
+    // };
+    
+    // setTimeout(() => {
+    //     $('.carousel-item')
+    // }, 10000);
+
+    // var carouselItem = document.getElementsByClassName('carousel-item');
+    
+    // carouselItem.classList.add('adf');
+    // console.log("movein");
+    // var carouselCaption = document.getElementsByClassName('.carousel-pation');
+    
+    // if (condition) {
+        
+    // }
 
 
 });
