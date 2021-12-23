522743f48557ea91e25f05a44d52199644e5d192
kazam zahedi <kazam.zahedi@gmail.com>
2021-12-23 11:22:56 +0100

    set move item in slideshow

--- script/script.js
+++ script/script.js
@@ -7,38 +7,5 @@ $(document).ready(function() {
     });
     
     $(".carousel-caption").addClass("moveselector");
-    // const myTimeout = setTimeout(moveTitle, 10000);
-    // var carouselItem = docoment.querySelector('.carousel-item');
-    // function moveTitle() {
-
-    //     // if (carouselItem == "active") {
-    //     //     carouselItem.style.opacity = "0.1"; 
-    //     //     console.log("hi");
-    //     // }
-    //     console.log("hi");
-
-        
-
-
-    // for (let index = 0; index < array.length; index++) {
-    //     const element = array[index];
-            
-    // }
-    // };
     
-    // setTimeout(() => {
-    //     $('.carousel-item')
-    // }, 10000);
-
-    // var carouselItem = document.getElementsByClassName('carousel-item');
-    
-    // carouselItem.classList.add('adf');
-    // console.log("movein");
-    // var carouselCaption = document.getElementsByClassName('.carousel-pation');
-    
-    // if (condition) {
-        
-    // }
-
-
 });
